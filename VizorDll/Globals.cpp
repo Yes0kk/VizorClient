5936a51c5d5363f76ef06c05dee52898cd4127d6
maxim <maxim@KERNELDEV>
2025-07-28 12:56:45 -0700

    uhhh adding the actual driver

---
+++ VizorDll/Globals.cpp
@@ -0,0 +1,4 @@
+// Globals.cpp
+#include "pch.h"
+
+extern "C" __declspec(dllexport) PVOID g_CommBuffer = nullptr;
--- VizorDll/dllmain.cpp
+++ VizorDll/dllmain.cpp
@@ -1,27 +1,16 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <Windows.h>
 
-void InitializeVizor()
-{
-    // Initialization code for Vizor can be placed here.
-    // For example, setting up logging, loading resources, etc.
-	MessageBoxA(NULL, "Vizor Initialized", "Info", MB_OK | MB_ICONINFORMATION);
-}
+extern "C" __declspec(dllexport) PVOID g_CommBuffer;
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
+    if (reason == DLL_PROCESS_ATTACH)
     {
-    case DLL_PROCESS_ATTACH:
-        MessageBoxA(NULL, "Vizor DLL Loaded", "Info", MB_OK | MB_ICONINFORMATION);
-		break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+        if (g_CommBuffer)
+        {
+            *((ULONG64*)g_CommBuffer) = 0x123456;
+        }
     }
     return TRUE;
 }
